7118fb5d22b93c357720cb1b9c0272facf6fb9de
Thomi7 <fabithomi@web.de>
2020-08-26 20:46:30 +0200

    fixed config creation on linux

--- ParanoidOneDriveBackup/Helper.cs
+++ ParanoidOneDriveBackup/Helper.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Reflection;
 using System.Text;
 
 namespace ParanoidOneDriveBackup
 {
-    static class Helper
+    internal static class Helper
     {
         public static string GetDescription(Type type)
         {
             var descriptions = (DescriptionAttribute[])type.GetCustomAttributes(typeof(DescriptionAttribute), false);
 
-            if (descriptions.Length == 0)
-                return null;
-            else
-                return descriptions[0].Description;
+            return descriptions.Length == 0 ? null : descriptions[0].Description;
+        }
+
+        public static Version? GetAppVersion() 
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version;
         }
     }
 }
--- ParanoidOneDriveBackup/Program.cs
+++ ParanoidOneDriveBackup/Program.cs
@@ -6,27 +6,48 @@ using ParanoidOneDriveBackup.App;
 using System;
 using System.IO;
 using System.Reflection;
+using Microsoft.Extensions.FileProviders;
 
 namespace ParanoidOneDriveBackup
 {
-    class Program
+    internal static class Program 
     {
 
         public static void Main(string[] args)
         {
+            Console.WriteLine("Version: {0}", Helper.GetAppVersion());
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseSystemd()
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     if (!File.Exists(Constants.CONFIG_FILE_PATH))
                     {
-                        File.Copy(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Resources", Constants.CONFIG_FILE_NAME), Constants.CONFIG_FILE_PATH);
+                        var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+                        Directory.CreateDirectory(Constants.APP_DATA_FOLDER_PATH);
+                        
+                        // copy config file
+                        using (var reader = embeddedProvider.GetFileInfo(Constants.CONFIG_FILE_NAME).CreateReadStream())
+                        {
+                            var fileStream = File.Create(Constants.CONFIG_FILE_PATH);
+                            reader.Seek(0, SeekOrigin.Begin);
+                            reader.CopyTo(fileStream);
+                            fileStream.Close();
+                        }
+                        
                         if (!File.Exists(Constants.IGNORE_FILE_PATH))
-                            File.Copy(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Resources", Constants.IGNORE_FILE_NAME), Constants.IGNORE_FILE_PATH);
+                        {
+                            // copy ignore file
+                            using var reader = embeddedProvider.GetFileInfo(Constants.IGNORE_FILE_NAME).CreateReadStream();
+                            var fileStream = File.Create(Constants.IGNORE_FILE_PATH);
+                            reader.Seek(0, SeekOrigin.Begin);
+                            reader.CopyTo(fileStream);
+                            fileStream.Close();
+                        }
+
                         Console.WriteLine($"No config file found. The default config file has been copied to \"{Constants.CONFIG_FILE_PATH}\". Modify it and restart the application.");
                         Console.WriteLine();
                         Console.WriteLine("Press any key to exit...");
